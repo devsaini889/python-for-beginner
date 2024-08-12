7d75c62cc6372f208273e778b4b3d0387264bb75
devsaini889 <ds0752607@gmail.com>
2024-08-12 22:57:11 +0530

    eighth

---
+++ 8evenorodd.py
@@ -0,0 +1,8 @@
+# A simple program to check weather a number is even or odd
+
+a = int(input("Enter the number: "))
+
+if a/2==True:
+    print("The number is even")
+else:
+    print("The number is odd")
