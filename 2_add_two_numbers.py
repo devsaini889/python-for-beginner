85acd43024e0fb48d9958967f131db23336e2542
devsaini889 <ds0752607@gmail.com>
2024-08-13 22:38:24 +0530

    second

---
+++ 2_add_two_numbers.py
@@ -0,0 +1,15 @@
+#Day 2
+# In first method we directly assign the number and add them
+a=3
+b=4
+c=a+b
+print(c)
+
+
+# In second method we take the input from the user and then print the sum
+
+a = int(input("Enter the first digit: "))
+b= int(input("Enter the second digit:"))
+
+c=a+b
+print(c)
