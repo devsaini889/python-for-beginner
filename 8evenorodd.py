6ae7b52d0b3dc7219190b6be3422d2a07e99f229
Dev Saini <149680766+devsaini889@users.noreply.github.com>
2024-08-13 22:43:27 +0530

    Delete 8evenorodd.py

--- 8evenorodd.py
+++
@@ -1,8 +0,0 @@
-# A simple program to check weather a number is even or odd
-
-a = int(input("Enter the number: "))
-
-if a/2==True:
-    print("The number is even")
-else:
-    print("The number is odd")
