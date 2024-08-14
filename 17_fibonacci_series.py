c6f14dc11afddd34d3df4b121d6bd89765644ef3
devsaini889 <ds0752607@gmail.com>
2024-08-14 22:18:25 +0530

    seventeen

---
+++ 17_fibonacci_series.py
@@ -0,0 +1,16 @@
+# A simple program to print the fibonacci series 
+
+a = 0
+b = 1
+num = int(input("Enter the number:"))
+if num==1:
+    print(a)
+
+else:
+    print(a)
+    print(b)
+    for i in range(2,num):
+        c=a+b
+        a=b
+        b=c
+        print(c)
