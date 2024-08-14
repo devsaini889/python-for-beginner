bca5fc674c5a2fe48c1fcaccd278775064508c4d
devsaini889 <ds0752607@gmail.com>
2024-08-14 22:48:36 +0530

    nineteen

---
+++ 19_armstrong_num_in_interval.py
@@ -0,0 +1,16 @@
+# A simple program to print the armstrong number in the given interval
+
+lower = int(input("Enter the lower limit:"))
+upper = int(input("Enter the upper limit:"))
+
+for num in range(lower,upper+1):
+    order= len(str(num))
+    sum = 0 
+    temp = num
+    while temp>0:
+        digit= temp%10
+        sum += digit**order
+        temp//=10
+
+    if num==sum:
+        print(num)
