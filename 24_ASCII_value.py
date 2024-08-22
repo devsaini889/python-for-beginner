111ad86bb1d37cfe60a4e1b5f9998fd76166bd29
devsaini889 <ds0752607@gmail.com>
2024-08-22 21:40:55 +0530

    twenty four

---
+++ 24_ASCII_value.py
@@ -0,0 +1,7 @@
+# A simple program to determine the ASCII value of a character
+
+ascii= str(input("Enter a character for which you want a ascii value: "))
+
+# For printing the ASCII value we use a builtin keyword in python known as ord 
+
+print("the ASCII value of the character is :", ord(ascii))
