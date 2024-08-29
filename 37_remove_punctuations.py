3241943cd9c2df16443c6c1bd5543c6e50283c80
devsaini889 <ds0752607@gmail.com>
2024-08-29 22:48:37 +0530

    Thirty Seven

---
+++ 37_remove_punctuations.py
@@ -0,0 +1,14 @@
+# A simple program to remove the punctuation in a string 
+
+punc = '''!@#$&*/.,';`~'''
+
+string = str(input("Enter the string: "))
+
+new_str= ''
+
+for i in string:
+    if i not in punc:
+        new_str+=i
+
+
+print(new_str)
